6773c6aee956fe96e2d763d33b7c66dfb8f84bd8
sebastian <isurus.1313@gmail.com>
2025-03-11 14:20:23 -0300

    add truncate

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,18 @@ import { ProductListComponent } from '../../components/product-list/product-list
 import { ProductTableComponent } from '../../components/product-table/product-table.component';
 import { CommonModule } from '@angular/common';
 
+//Importar módulos de Angular Material correctamente
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+// Pipe para truncar texto
+import { TruncatePipe } from '../../pipes/truncate.pipe';
+
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -18,12 +30,22 @@ import { CommonModule } from '@angular/common';
     SideMenuComponent,
     // ProductListComponent,
     ProductTableComponent,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    TruncatePipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   products: Product[] = [];
+  terminoBusqueda: string = '';
+  productosFiltrados: Product[] = [];
+  inicio: number = 0;
+  itemsPorPagina: number = 4; // Cantidad de tarjetas visibles
 
   constructor(
     private productService: ProductService,
@@ -31,17 +53,33 @@ export class HomeComponent {
   ) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.loading.setIsLoading(true);
-
-      this.productService.getProducts().subscribe((data) => {
-        console.log('Productos recibidos:', data);
+    this.productService.getProducts().subscribe({
+      next: (data: Product[]) => {
         this.products = data;
-        this.loading.setIsLoading(false);
-      });
-    }, environment.pageInitialStartLoadingServiceDelay);
+        this.productosFiltrados = [...this.products]; // Inicialmente, mostrar todos los productos
+      },
+      error: (err) => console.error('Error al obtener productos:', err),
+    });
   }
 
+  siguiente(): void {
+    if (this.inicio + this.itemsPorPagina < this.productosFiltrados.length) {
+      this.inicio += this.itemsPorPagina;
+    }
+  }
 
+  anterior(): void {
+    if (this.inicio > 0) {
+      this.inicio -= this.itemsPorPagina;
+    }
+  }
 
+  filtrarProductos(termino: string): void {
+    this.inicio = 0; // Reiniciar índice al filtrar
+    this.productosFiltrados = this.products.filter(product =>
+      product.category.name.toLowerCase().includes(termino.toLowerCase())
+    );
+  }
 }
+
+
