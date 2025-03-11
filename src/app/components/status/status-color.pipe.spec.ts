8890e9b5ce6e91cb571c1efa24334ae3c7225ecb
sebastian <isurus.1313@gmail.com>
2025-03-11 19:25:18 -0300

    update fixed

--- src/app/components/status/status-color.pipe.spec.ts
+++ src/app/components/status/status-color.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { StatusColorPipe } from './status-color.pipe';
+import { StatusColor } from './status-color';
 
 describe('StatusColorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatusColorPipe();
+  let pipe: StatusColorPipe;
+
+  beforeEach(() => {
+    pipe = new StatusColorPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform a function that returns StatusColor', () => {
+    // Simulamos una función que devuelve un StatusColor según el argumento
+    const mockFunction = (status: string): StatusColor => {
+      return status === 'success' ? StatusColor.success : StatusColor.error;
+    };
+
+    expect(pipe.transform(mockFunction, 'success')).toBe(StatusColor.success);
+    expect(pipe.transform(mockFunction, 'fail')).toBe(StatusColor.error);
   });
 });
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { provideHttpClient } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+
+  let router: Router; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,4 +23,18 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an empty products array initially', () => {
+    expect(component.products).toBeDefined();
+    expect(component.products.length).toBe(0);
+  });
+
+
+
+beforeEach(() => {
+  router = TestBed.inject(Router);
+  spyOn(router, 'navigate');
+});
+
+
 });
